9eac0deb87312d1e8c588bc9555be170e219a8ac
William Tang <wjt40398@gmail.com>
2024-02-20 18:08:52 -0500

    Restructure Not-found page

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,5 +1,10 @@
+import { AppShellMain } from '@mantine/core';
 import { NothingFoundBackground } from '@/components/NothingFoundBackground/NothingFoundBackground';
 
 export default function NotFound() {
-  return <NothingFoundBackground />;
+  return (
+    <AppShellMain>
+      <NothingFoundBackground />
+    </AppShellMain>
+  );
 }
