08c71b4b4cb4ef719bc6578e51c1868824857d6b
William Tang <wjt40398@gmail.com>
2024-02-22 17:37:04 -0500

    Railway Config

--- src/frontend/.eslintrc.cjs
+++ src/frontend/.eslintrc.cjs
@@ -1,5 +1,5 @@
 module.exports = {
-  extends: ['mantine', 'plugin:@next/next/recommended', 'plugin:jest/recommended'],
+  extends: ['mantine', 'plugin:@next/next/recommended', 'plugin:jest/recommended', 'prettier'],
   plugins: ['testing-library', 'jest'],
   overrides: [
     {
